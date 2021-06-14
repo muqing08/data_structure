0d239cf28d5aff2dba010e795234559d26526b9a
lyonmu <lyonmu@foxmail.com>
2021-06-14 10:14:22 +0800

    init project

--- C_Unit_1/mian.c
+++ C_Unit_1/mian.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "question1.h"
-int main()
-{
+
+int main() {
     int a, b, c;
     printf("hello world\n");
-    scanf("%d %d %d\n", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
     question1(a, b, c);
     return 0;
 }
