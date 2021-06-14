ee468b7f8d012b1b9680fa7f07d3b426796a63ec
lyonmu <lyonmu@foxmail.com>
2021-06-14 16:35:05 +0800

    add c_1.3

--- C_Unit_1/mian.c
+++ C_Unit_1/mian.c
@@ -2,10 +2,13 @@
 #include "question.h"
 
 int main() {
-    int a, b, c;
+//    int a, b, c;
+    int number;
     printf("hello world\n");
 //    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d", &number);
 //    question1(a, b, c);
-    question2();
+//    question2();
+    question3(number);
     return 0;
 }
--- C_Unit_1/question.h
+++ C_Unit_1/question.h
@@ -5,9 +5,19 @@ int question1(int a, int b, int c) {
     return 1;
 }
 
-int question2(){
+int question2() {
     printf("第1行\t:\t姓名\n");
     printf("第二行\t:\t门牌号，街道\n");
     printf("第三行\t:\t城市名，邮政编号\n");
+    return 1;
+}
+
+int question3(int number) {
+    int i = 1;
+    while (i <= 10) {
+        printf("%d\tx\t%d\t=\t%d\n", number, i, number * i);
+        i++;
+    }
+
     return 1;
 }
